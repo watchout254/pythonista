fbc923a3b7254171063c09e1c89d1113d1f0eccb
Daniel Mukenya <88248852+watchout254@users.noreply.github.com>
2023-09-27 13:23:14 +0300

    daily

--- sets.py
+++ sets.py
@@ -3,3 +3,24 @@ set1.pop()
 print(set1.pop())
 print(set1)
 print(len(set1))
+
+
+a = 5
+b = 4
+
+print(a&b)
+print(a | b)
+print(a^b)
+print(~a)
+print(~b)
+print(a<<2)
+print(a>>2)
+print("")
+print("")
+
+print(26&23)
+print(17|24)
+print(17^24)
+print(~45)
+print(68<<2)
+print(56>>3)
