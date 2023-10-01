93df9bf7cd1d116edb252fd914bd8907c8fb2753
Daniel Mukenya <88248852+watchout254@users.noreply.github.com>
2023-10-01 09:22:48 +0300

    yohh

---
+++ func_arg.py
@@ -0,0 +1,16 @@
+#Types of arguments
+#default, positional, keyword, arbitrory
+
+
+def greet(name):
+    print(f"Hi {name}")
+    print(f"Are you from Information Technology Department")
+
+greet("Daniel")
+greet("mukenya")
+
+def add(a,b,d):
+    c = a+b+d
+    print(f"Sum is {c}")
+add(5,7,8)
+add(8,9,3)
