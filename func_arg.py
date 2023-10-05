1b84533a333f6591112a76d345fabc35a0767115
Daniel Mukenya <88248852+watchout254@users.noreply.github.com>
2023-10-05 11:11:12 +0300

    daily

--- func_arg.py
+++ func_arg.py
@@ -14,3 +14,19 @@ def add(a,b,d):
     print(f"Sum is {c}")
 add(5,7,8)
 add(8,9,3)
+
+
+def add(a,b):
+    c = a+b
+    #print(c)
+    return c
+result = (add(5,4))
+print(result)
+
+
+def format_name(first,last):
+    fmat = first.title()
+    fnat = last.title()
+    return f"{fmat} {fnat}"
+      
+print(format_name("daniel","MUKENYA"))
