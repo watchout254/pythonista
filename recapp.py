53f24a6b8e6bbfad7004fd617406b496ca07a252
Daniel Mukenya <88248852+watchout254@users.noreply.github.com>
2023-10-16 10:52:29 +0300

    fine

---
+++ recapp.py
@@ -0,0 +1,14 @@
+#swap two numbers
+
+"""a = input("Enter value of a:  ")
+b = input("Enter value of b: ")
+
+temp = a
+a = b
+b = temp
+
+print("a " +a)
+print("b "+b) """
+
+addition = input("Enter a two digit number: ")
+print(int(addition[0])+ int(addition[1]))
