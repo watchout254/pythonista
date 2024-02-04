e9268fb72ec0f3601396f26757b579830b25e2de
Daniel Mukenya <88248852+watchout254@users.noreply.github.com>
2024-02-04 21:45:39 +0300

    Awooo

--- breakconpass.py
+++ breakconpass.py
@@ -1,22 +1,22 @@
-#count=1
-#while count <= 10:
-    #print(count)
-    #count += 1
+count=1
+while count <= 10:
+    print(count)
+    count += 1
 
-    #if count == 7:
-    #    continue
-    #print("Hi")
-#print("Out of loop")
+    if count == 7:
+       continue
+    print("Hi")
+print("Out of loop")
 
-#list1 = ["hi","hello","Welcome"]
-#names = ["krishnn","Ram","madhav"]
-#for item in list1:
-#    for name in names:
-#       print(item,name)
- #       if item == "hello" and name == "Ram":
-      #      break
- #   print("out from inner loop")
-#print("out of outer loop")
+list1 = ["hi","hello","Welcome"]
+names = ["krishnn","Ram","madhav"]
+for item in list1:
+    for name in names:
+       print(item,name)
+       if item == "hello" and name == "Ram":
+           break
+    print("out from inner loop")
+print("out of outer loop")
 for i in range(1,11):
     if i == 7:
         continue
